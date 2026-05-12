336cee9b6743890b61c08004564bbedcad2055e3
smccaffery0 <sundikarenia@gmail.com>
2026-05-12 14:30:22 -0500

    fixed previous commit issue

--- src/table.rs
+++ src/table.rs
@@ -5,7 +5,7 @@ use core::error;
 use csv::{self};
 use serde_derive::{Deserialize, Serialize};
 use std::{
-    io::{self},
+    io::{self, stdin},
     process::exit,
 };
 
@@ -19,45 +19,27 @@ pub struct AnimeSheet {
     //pub anime_status: String,
 }
 
-pub enum MyError {
-    NotValidOption,
-}
-
-pub fn add_row() -> Result<AnimeSheet, MyError> {
-    println!("Do want to enter another title: ");
-    let mut add_more = String::new();
-    let _ = io::stdin().read_line(&mut add_more);
-    let add_more = add_more.trim();
-    match add_more {
-        "y" => Ok(fill_table()),
-        "n" => exit(0),
-        _ => Err(MyError::NotValidOption),
-    }
-}
-
 // Take user input for filling out the spreadsheet
 pub fn fill_table() -> AnimeSheet {
     println!("Enter anime count: ");
     let mut anime_count = String::new();
-    let _ = io::stdin().read_line(&mut anime_count);
+    let _ = stdin().read_line(&mut anime_count);
     let anime_count = anime_count.trim().to_string();
 
     println!("Enter anime title: ");
     let mut anime_title = String::new();
-    let _ = io::stdin().read_line(&mut anime_title);
+    let _ = stdin().read_line(&mut anime_title);
     let anime_title = anime_title.trim().to_string();
 
     println!("Enter episode count: ");
     let mut episode_count = String::new();
-    let _ = io::stdin().read_line(&mut episode_count);
+    let _ = stdin().read_line(&mut episode_count);
     let episode_count = episode_count.trim().to_string();
 
     println!("\nEnter your rating: ");
     let mut user_rating = String::new();
-    let _ = io::stdin().read_line(&mut user_rating);
+    let _ = stdin().read_line(&mut user_rating);
     let user_rating = user_rating.trim().to_string();
-
-    add_row();
 
     //TODO! //Create another column for progress/status
     //println!("\nEnter your rating: ");
@@ -77,42 +59,59 @@ pub fn fill_table() -> AnimeSheet {
 
 // Define the Table
 pub fn create_table() -> Result<(), Box<dyn error::Error>> {
-    let sheet = fill_table();
+    println!("Do want to enter a title: ");
+    let mut add_more = String::new();
+    let _ = io::stdin().read_line(&mut add_more);
+    let add_more = add_more.trim();
+    if add_more == "y" {
+        loop {
+            let sheet = fill_table();
+            let mut anime_table = Table::new();
 
-    let mut anime_table = Table::new();
+            //Write and save to file
+            let file = std::fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open("anime.csv")?; // this only work if you are in src directory
 
-    //Write and save to file
-    let file = std::fs::OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("anime.csv")?;
+            let mut wtr = csv::WriterBuilder::new()
+                .has_headers(false)
+                .from_writer(file);
 
-    let mut wtr = csv::WriterBuilder::new()
-        .has_headers(false)
-        .from_writer(file);
+            wtr.serialize(&sheet)?;
+            wtr.flush()?;
 
-    wtr.serialize(&sheet)?;
-    wtr.flush()?;
+            // defines table properties
+            anime_table
+                .load_preset(UTF8_FULL)
+                .apply_modifier(UTF8_ROUND_CORNERS)
+                .set_content_arrangement(comfy_table::ContentArrangement::DynamicFullWidth)
+                .set_header(vec!["Count", "Title", "Episodes", "Rating"]);
 
-    // defines table properties
-    anime_table
-        .load_preset(UTF8_FULL)
-        .apply_modifier(UTF8_ROUND_CORNERS)
-        .set_content_arrangement(comfy_table::ContentArrangement::DynamicFullWidth)
-        .set_header(vec!["Count", "Title", "Episodes", "Rating"]);
-
-    let rows = vec![
-        &sheet.anime_count,
-        &sheet.anime_title,
-        &sheet.episode_count,
-        &sheet.user_rating,
-        //&sheet.anime_status,
-    ];
-    anime_table.add_row(rows);
-    Ok(())
+            let rows = vec![
+                &sheet.anime_count,
+                &sheet.anime_title,
+                &sheet.episode_count,
+                &sheet.user_rating,
+                //&sheet.anime_status,
+            ];
+            anime_table.add_row(rows);
+            println!("Do want to enter another title: ");
+            let mut add_again = String::new();
+            let _ = io::stdin().read_line(&mut add_again);
+            let add_again = add_again.trim();
+            if add_again == "y" {
+                fill_table();
+            } else {
+                exit(0)
+            }
+        }
+    } else {
+        exit(0);
+    }
 }
 
 pub fn draw_table() -> Result<(), Box<dyn std::error::Error>> {
-    //TODO! //read and print out table
+    //TODO Read csv and  print it out to terminal
     Ok(())
 }
